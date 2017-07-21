6cafce531422aefba74b6511609111ab3a5c1df0
hopeverse <hopeverse@gmail.com>
2017-07-21 18:11:58 +0800

    update add js

---
+++ js/my.js
@@ -0,0 +1,25 @@
+
+
+window.onload=function()
+{
+	var oCh = document.getElementById('ch');
+	var oChip = document.getElementById('chip');
+	var oIp = oChip.getElementsByTagName('input');
+	var oChdiv = document.getElementById('chdiv');
+	var aChdiv = oChdiv.getElementsByTagName('div');
+
+	for(var i=0;i<oIp.length;i++)
+	{
+		oIp[i].index = i;
+		oIp[i].onclick=function()
+		{
+			for(var i=0;i<oIp.length;i++)
+			{
+				oIp[i].className="init-btn-class";
+				aChdiv[i].style.display="none";
+			}
+			this.className="active";
+			aChdiv[this.index].style.display="block"
+		}
+	}
+}
